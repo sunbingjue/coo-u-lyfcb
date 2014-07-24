8ad4031be16d85d480dfefacaf8583ea83173854
Zhang, Shulai <kikyou1986@hotmail.com>
2014-07-24 15:39:38 +0800

    ff

--- src/main/java/coo/u/lyfcb/Hello.java
+++ src/main/java/coo/u/lyfcb/Hello.java
@@ -4,11 +4,6 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("zsl");
-		System.out.println("sbj");
-<<<<<<< HEAD
-		System.out.println("bbb");
-=======
->>>>>>> branch 'master' of https://github.com/sunbingjue/coo-u-lyfcb.git
+		
 	}
 }
